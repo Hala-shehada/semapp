72af7a9b1929240fea9cbbfb6b42aa68cf3c0439
Hala <hala.sayel.sh@gmail.com>
2022-10-30 07:32:44 +0200

    setup

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const Port = process.env.PORT || 5000;
 const app = express();
+require('dotenv').config();
 
-mongoose.connect("mongodb+srv://halanuha:QKkdp2tuhnPF45j8@appdb.u8evh6i.mongodb.net/<SemDB>?retryWrites=true&w=majority",{ useNewUrlParser: true, useUnifiedTopology: true });
-
+// const mongoDbURL = process.env.mongoDbURL || 'mongodb+srv://halanuha:QKkdp2tuhnPF45j8@appdb.u8evh6i.mongodb.net/<SemDB>?retryWrites=true&w=majority'
+// mongoose.connect("mongodb+srv://halanuha:QKkdp2tuhnPF45j8@appdb.u8evh6i.mongodb.net/<SemDB>?retryWrites=true&w=majority",{ useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true } )
 const connection = mongoose.connection;
 connection.once("open", ()=>{
     console.log("MongoDb connected");
